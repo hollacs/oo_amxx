f13be13b692c17d4728b4b51db17eb9626d79d5e
colgay <vip-colgate@hotmail.com>
2024-06-28 11:33:20 +0800

    remove some util functions (use oo_forward instead)

--- inc/oo_utils.h
+++ inc/oo_utils.h
@@ -2,19 +2,12 @@
 #define OO_UTILS_H
 
 #include <amtl/am-vector.h>
+#include <amtl/am-autoptr.h>
 #include "amxxmodule.h"
 
 namespace oo {
 namespace util
 {
-    using AmxxForward = int32_t;
-    using ArgList = ke::Vector<int8_t>;
-    using HookChain = ke::Vector<AmxxForward>;
-    using ObjectHash = size_t;
-
-    AmxxForward AddMethod(AMX *amx, const char *callback, const ArgList *args=nullptr);
-    cell ExecuteMethod(AMX *amx, cell *params, AmxxForward forward_id, const ArgList* args=nullptr, int8_t start_param=0);
-    cell ExecuteMethodHookChain(AMX *amx, cell *params, const HookChain *hook_chain, const ArgList *args=nullptr, int8_t start_param=0);
     int clamp(int value, int lower, int upper);
 }}
 
